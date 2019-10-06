f4ec7b7e9f5713ddfed76900404e9128faebd07d
Ivan Kapelyukh <ivan.kapelyukh@gmail.com>
2019-10-06 14:24:20 +0100

    Print a board's dimensions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::fmt;
+
 fn main() {
-    println!("Hello, world!");
+  let board = Board {
+    width: 10,
+    height: 12,
+  };
+  println!("{}", board);
+}
+
+struct Board {
+  width: u32,
+  height: u32,
+}
+
+impl fmt::Display for Board {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    write!(f, "{} by {}", self.width, self.height)
+  }
 }
