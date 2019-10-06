bc1657711b54963708a9e659135f45ffb2223558
Ivan Kapelyukh <ivan.kapelyukh@gmail.com>
2019-10-06 15:35:49 +0100

    Add row and col headers

--- src/main.rs
+++ src/main.rs
@@ -13,9 +13,17 @@ struct Board {
 
 impl fmt::Display for Board {
   fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    // Write col headers.
+    write!(f, " ")?;
+    for c in (0..self.cells.len()).map(|i| (b'a' + (i as u8)) as char) {
+      write!(f, " {}", c)?;
+    }
+    writeln!(f)?;
+
     for (i, row) in self.cells.iter().enumerate() {
-      for (j, cell) in row.iter().enumerate() {
-        write!(f, "{}", cell)?;
+      write!(f, "{}", i + 1)?; // Write row headers.
+      for cell in row.iter() {
+        write!(f, " {}", cell)?;
       }
       writeln!(f)?;
     }
