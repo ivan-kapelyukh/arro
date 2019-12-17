0303849372b1b8be3a61fefd1a65e5ae2bd8abb5
Ivan Kapelyukh <ivan.kapelyukh@gmail.com>
2019-12-17 15:42:22 +0000

    Implement Piece enum

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,56 @@
-use std::fmt;
+use std::fmt::Display;
+use std::fmt::Formatter;
+use std::fmt::Result;
 
 fn main() {
-  let board = Board {
-    cells: [[0, 1, 2], [3, 4, 5], [6, 7, 8]],
-  };
-  println!("{}", board);
+    let board = Board {
+        cells: [
+            [Piece::Cross, Piece::Empty, Piece::Nought],
+            [Piece::Cross, Piece::Empty, Piece::Nought],
+            [Piece::Cross, Piece::Empty, Piece::Nought],
+        ],
+    };
+    println!("{}", board);
 }
 
 struct Board {
-  cells: [[u32; 3]; 3],
+    cells: [[Piece; 3]; 3],
 }
 
-impl fmt::Display for Board {
-  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-    // Write col headers.
-    write!(f, " ")?;
-    for c in (0..self.cells.len()).map(|i| (b'a' + (i as u8)) as char) {
-      write!(f, " {}", c)?;
-    }
-    writeln!(f)?;
+#[derive(Copy, Clone)]
+enum Piece {
+    Empty,
+    Nought,
+    Cross,
+}
 
-    for (i, row) in self.cells.iter().enumerate() {
-      write!(f, "{}", i + 1)?; // Write row headers.
-      for cell in row.iter() {
-        write!(f, " {}", cell)?;
-      }
-      writeln!(f)?;
+impl Display for Piece {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        let character = match self {
+            Piece::Empty => ' ',
+            Piece::Nought => 'O',
+            Piece::Cross => 'X',
+        };
+        write!(f, "{}", character)
     }
-    Ok(())
-  }
+}
+
+impl Display for Board {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        // Write col headers.
+        write!(f, " ")?;
+        for c in (0..self.cells[0].len()).map(|i| (b'a' + (i as u8)) as char) {
+            write!(f, " {}", c)?;
+        }
+        writeln!(f)?;
+
+        for (i, row) in self.cells.iter().enumerate() {
+            write!(f, "{}", i + 1)?; // Write row headers.
+            for cell in row.iter() {
+                write!(f, " {}", cell)?;
+            }
+            writeln!(f)?;
+        }
+        Ok(())
+    }
 }
